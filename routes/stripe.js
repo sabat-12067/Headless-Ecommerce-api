a046988d64baa948a0d81fe0c68322e12b2f38f9
sabat-12067 <greatdev12067@gmail.com>
2022-06-02 17:29:30 +0300

    edit stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-const collectStripePayment = require('../controllers/stripe');
-
-router.post('/payment', collectStripePayment);
+router.post('/payment-intent', async (req, res) => {
+  const { cartItem } = req.body;
+  const { price, email } = cartItem;
+  try {
+   const paymentIntent = await stripe.paymentIntents.create({
+     amount: price * 100,
+     currency: 'usd',
+     payment_method_types: ['card'],
+     receipt_email: email,
+     description: 'Asuman Sounds Store',
+   });
+   res.status(200).json({
+     clientSecret: paymentIntent.client_secret,
+   });   
+  } catch (error) {
+   res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
