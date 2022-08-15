ae4385fc197c5f5baee9c5cd75bb02379f933676
sabat-12067 <greatdev12067@gmail.com>
2022-08-15 20:57:38 +0300

    commit changes

--- routes/validateToken.js
+++ routes/validateToken.js
@@ -2,27 +2,11 @@ const jwt = require("jsonwebtoken");
 const { StatusCodes } = require('http-status-codes');
 const router = require('express').Router();
 
-// router.get('/validate-token', (req, res) => {
-//  const authHeader = req.headers["authorization"];
-//  const token = authHeader && authHeader.split(" ")[1];
-
-//  if (token == null) return res.sendStatus(401).json({ msg: 'Invalid token' });
-
-//  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-//   if (err) return res.sendStatus(403);
-//   req.tokenData = decoded;
-//   res.status(StatusCodes.OK).json({ msg: 'Token verified' })
-//  }); 
-// });
-
 router.post('/validate-token', (req, res) => {
- //const bearerHeader = req.headers.token;
  const bearerToken = req.body.token;
 
  if (typeof bearerToken !== 'undefined') {
-  // const bearerToken = bearerHeader.split(' ')[1];
-  jwt.verify(bearerToken, process.env.JWT_SECRET, (err, authData) => {
-   //if token is provided but invalid
+  jwt.verify(bearerToken, process.env.JWT_SECRET, (err) => {
    if (err) {
     res.status(StatusCodes.FORBIDDEN).json({
      message: 'Invalid token!'
@@ -37,7 +21,6 @@ router.post('/validate-token', (req, res) => {
   });
  }
 }
-)
-
+);
 
 module.exports = router;
